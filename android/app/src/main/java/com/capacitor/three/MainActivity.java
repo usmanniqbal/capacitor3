adda13e531b89718a40ebb76946ae86fb1f39c60
Usman Iqbal <usman@eventmobi.com>
2021-06-15 19:08:12 -0400

    general changes

--- android/app/src/main/java/com/capacitor/three/MainActivity.java
+++ android/app/src/main/java/com/capacitor/three/MainActivity.java
@@ -1,5 +1,18 @@
 package com.capacitor.three;
 
 import com.getcapacitor.BridgeActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    AppCenter.start(getApplication(), "b01cf82e-4c4d-495c-9027-332ea10819e3",
+                  Analytics.class, Crashes.class);
+  }
+}
