725bb5b4324556598353c1cd2ea0ee7974837903
Usman Iqbal <usman@eventmobi.com>
2021-06-15 19:28:51 -0400

    general changes

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -1,6 +1,8 @@
-npm i
+cd ../../
+echo 'directory changed'
+npm i 
 echo 'npm install done'
-yarn build
+yarn build 
 echo 'yarn build done'
-npx cap sync android
+npx cap sync android 
 echo 'cap sync android done'
