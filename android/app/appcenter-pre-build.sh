3141bded0cf1f3af985db6131d0738009a5a97e7
Usman Iqbal <usman@eventmobi.com>
2021-06-17 18:09:26 -0400

    general changes

--- android/app/appcenter-pre-build.sh
+++ android/app/appcenter-pre-build.sh
@@ -5,6 +5,8 @@ echo 'npm install done'
 yarn build 
 echo 'yarn build done'
 curl -o resources/android/icon.png --create-dirs  https://cricnix-fantasy-images.s3.ap-southeast-2.amazonaws.com/1623964277430-31320.png
+cp resources/android/icon.png resources/android/icon-foreground.png
+cp resources/android/icon.png resources/android/icon-background.png
 echo 'download icon'
 curl -o resources/android/splash.png --create-dirs https://cricnix-fantasy-images.s3.ap-southeast-2.amazonaws.com/1623964198514-51573.png
 echo 'download splash'
